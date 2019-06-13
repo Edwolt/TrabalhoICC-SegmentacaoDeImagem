376805fee563d85f1cd841bc2e3e9c6ed522ac7c
Eduardo Rocha <eduardos.rocha17@gmail.com>
2019-06-13 17:27:15 -0300

    Atualizando Filtro.c
    A função _conquista() está mais detalhada

--- filtro.c
+++ filtro.c
@@ -1,18 +1,59 @@
 #include "filtro.h"
 
-// Usei variáveeis locais para economizar espaço durante a recursão
-// Adote sempre deixar zerado após o uso em uma função
-int media = 0;
-int criterio = 0;
+/*
+ * Usei variáveeis gloabais para economizar memória durante a recursão de _conquista
+ * Pois esses valores teria que ser passado como parâmetro para na função recursica _conquista
+ * Para ficar padronizado, adote sempre deixar esses valores zerado após o usá-los em uma função
+ */
+int _quantidade = 0; // Para fazer a média
+int _total = 0;      // Para fazer a média
+int _criterio = 0;
+pixel **_imagem = NULL;
+int _altura = 0;
+int _largura = 0;
+int _valor = 0;
 
-// Função recursiva de conquista
-void _conquista()
+float media()
 {
+    return _total / ((float)_quantidade);
 }
 
-void filtro_conquista(pixel **imagem, int x, int y, int crit, const int valor)
+// Função recursiva de conquista
+void _conquista(int x, int y)
 {
-    criterio = crit;
-    _conquista();
-    criterio = media = 0;
+    // TODO Volta na recursão pois atingiu a borda da imagem
+
+    // TODO Processa pixel a cima
+
+    // TODO Processa pixel a direita
+
+    // TODO Processa pixel abaixo
+
+    // TODO Processa pixel a esquerda
+
+    // Recursão pixel a cima
+    _conquista(x, y - 1);
+
+    // Recursão pixel a direita
+    _conquista(x + 1, y);
+
+    // Recursão pixel abaixo
+    _conquista(x, y + 1);
+
+    // Recursão pixel a esquerda
+    _conquista(x - 1, y);
+}
+
+void filtro_conquista(pixel **imagem, int altura, int largura, int x, int y, int criterio, const int valor)
+{
+    _imagem = imagem;
+    _altura = altura;
+    _largura = largura;
+    _criterio = criterio;
+
+    // TODO Processa pixel (x, y) pois ele não será testado durante a recursão
+
+    _conquista(x, y);
+    _imagem = NULL;
+    _quantidade = _total = _altura = _largura = _criterio = 0;
 }
